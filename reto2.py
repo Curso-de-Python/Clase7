901b57fc2ab5554d604d9586e6c1ac5b07f7f8d3
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 15:18:15 -0500

    Create reto2.py

---
+++ reto2.py
@@ -0,0 +1,20 @@
+'''
+-----------------------------
+ RETO N°2
+ Números primos
+-----------------------------
+Un número natural es primo si es mayor que 1 y no tiene  divisores más que 1 y si mismo. Tu tarea es escribir una función que verifique si un número es primo o no.
+
+La función:
+ - Se llama esPrimo
+ - Toma un argumento (el valor a verificar)
+ - Devuelve True si el argumento es un número primo, y False de lo contrario
+-----------------------------
+'''
+def esPrimo(num):
+  # coloca tu código aquí
+
+for i in range(1, 20):
+    if esPrimo(i + 1):
+        print(i + 1, end=" ")
+print()
