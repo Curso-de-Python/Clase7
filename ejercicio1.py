1118a3a47be93facdc01cd7b45be175174ea5a74
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 15:16:12 -0500

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+'''
+-----------------------------
+ EJERCICIO N°1
+ Tu primera función
+-----------------------------
+ ¿Cómo transformar una parte de código que se está repitiendo en una función?
+-----------------------------
+'''
+print("Ingresa un valor: ")
+a = int(input())
+
+print("Ingresa un valor: ")
+b = int(input())
+
+print("Ingresa un valor: ")
+a = int(input())
