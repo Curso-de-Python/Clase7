31da86ed30e1d545ddf2715fde3d53bc79b7a8f9
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 15:15:56 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,14 @@
+# FUNCIONES CON PARÁMETROS
+
+def mensaje(numero):
+  print("Ingresa un número:", numero)
+
+mensaje() # ¿Qué ocurre aquí?
+
+# ---------------------------
+#def mensaje(numero):
+#  print("Ingresa un número:", numero)
+
+#numero = 1234
+#mensaje(1)
+#print(numero)
